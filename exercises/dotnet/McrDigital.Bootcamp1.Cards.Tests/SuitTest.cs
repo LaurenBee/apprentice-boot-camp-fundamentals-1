0ea0d7d10f1d317d4309fbc73f6ead7d1ee55fae
Lauren Baker <lauren_baker@waters.com>
2021-11-15 15:30:56 +0000

    Adding Suit Class

---
+++ exercises/dotnet/McrDigital.Bootcamp1.Cards.Tests/SuitTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace McrDigital.Bootcamp1.Cards.Tests
+{
+    public class SuitTest
+    {
+        [Fact]
+        public void CanCreateSuit()
+        {
+            var suit = new Suit(1, "Diamonds");
+
+            Assert.Equal(1, suit.Value);
+            Assert.Equal("Diamonds", suit.Name);
+        }
+    }
+}
---
+++ exercises/dotnet/McrDigital.Bootcamp1.Cards/Suit.cs
@@ -0,0 +1,14 @@
+﻿namespace McrDigital.Bootcamp1.Cards
+{
+    public class Suit
+    {
+        public Suit(int value, string name)
+        {
+            this.Value = value;
+            this.Name = name;
+        }
+
+        public int Value { get; }
+        public string Name { get; }
+    }
+}
