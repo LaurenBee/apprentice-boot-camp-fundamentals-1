c505e17aad162041021c25b0a73dc868e06b8404
Lauren Baker <lauren_baker@waters.com>
2021-11-15 14:53:02 +0000

    Adding PlayingCard Class

---
+++ exercises/dotnet/McrDigital.Bootcamp1.Cards.Tests/PlayingCardTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace McrDigital.Bootcamp1.Cards.Tests
+{
+    public class PlayingCardTest
+    {
+        [Fact]
+        public void CanCreatePlayingCard ()
+        {
+            var playingCard = new PlayingCard(0, 2);
+
+            Assert.Equal(0, playingCard.Suit);
+            Assert.Equal(2, playingCard.FaceValue);
+        }
+    }
+}
---
+++ exercises/dotnet/McrDigital.Bootcamp1.Cards/PlayingCard.cs
@@ -0,0 +1,13 @@
+﻿namespace McrDigital.Bootcamp1.Cards
+{
+    public class PlayingCard
+    {
+        public PlayingCard(int suit, int faceValue)
+        {
+            this.Suit = suit;
+            this.FaceValue = faceValue;
+        }
+        public int Suit { get; }
+        public int FaceValue { get; }
+    }
+}
